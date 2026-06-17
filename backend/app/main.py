8af3a8a13dd09ada096e88596dfa8506ff4acdb2
Kiran <kiranghumare5517@gmail.com>
2026-06-16 22:11:09 -0400

    Added frontend url to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://edu-rag-two.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
